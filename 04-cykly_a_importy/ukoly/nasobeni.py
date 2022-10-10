4157f072bcb668204f91c98d73d8b919b2deb8de
Petr Silhak <petrsilhak@gmail.com>
2022-10-10 07:18:06 +0200

    ukoly

---
+++ 04-cykly_a_importy/ukoly/nasobeni.py
@@ -0,0 +1,3 @@
+# Iterujte listem a každé číslo vynásobte číslem následujícím. Výsledek zapište opět do listu a vypište
+
+nums = [2, 4, 56, 7, 5, 10]
---
+++ 04-cykly_a_importy/ukoly_opakovani/cislo2.py
@@ -0,0 +1,4 @@
+# Udělejte hru, kde si počítač bude myslet jedno číslo a uživatel bude hádat jaké to číslo je. Počítat bude odpovídat jestli je myšlené číslo větší nebo menší
+# Pokud uživatel číslo uhádně, tak ho pochvalte
+
+
