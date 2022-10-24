c733b5ff2878fc1e258256d43c31c0eb647c831c
Petr Silhak <petrsilhak@gmail.com>
2022-10-24 09:42:49 +0200

    Kviz

---
+++ 05-funkce/kod_z_hodiny.py
@@ -0,0 +1,28 @@
+# 62
+a = {'key': 'value', 'key2': 'value2'}
+for k, v in a.items():
+    print(k, v)
+
+#48
+a = {"jméno": "Jan", "příjmení":"Horák"}
+a['vek'] = 20
+
+#49
+l = [
+    [1, 2, 3]
+    [4, 5, 6]
+    [7, 8, 9]
+]
+
+#55
+if not True:
+    pass
+
+#60
+a = {"jméno": "Jan", "příjmení":"Horák"}
+for k in a:
+    print(k)
+
+#47
+print(a['jméno'])
+a[1]
--- 05-funkce/quiz/zarad_cislo.py
+++ 05-funkce/quiz/zarad_cislo.py
@@ -1,1 +1,4 @@
-# Vytvořte kód, který najde pozice čísel v seznamu
+# Vytvořte kód, který zařadí pole čísel b do pole čísel a (vzestupně). Nakonec vypíše hromadně číslo z pole b a jeho novou pozici v poli a.
+
+a = [1, 54, 72, 88, 147, 231, 284]
+b = [11, 32, 98, 125, 300]
