766f5be41a21d1be3d7d2b01c73096add98a433f
Petr Silhak <petrsilhak@gmail.com>
2022-12-05 20:28:53 +0100

    IS ukol

--- 07-objektove_orientovane_programovani/ukoly_oop/information_system.py
+++ 07-objektove_orientovane_programovani/ukoly_oop/information_system.py
@@ -5,25 +5,23 @@ V tomto cviceni si vytvorime jednoduchy system pro ukladani informaci o lidech v
     - id (nahodne generovane cislo)
     - name (str)
     - surname (str)
-    - birthdate (str) ve formátu DD.MM.RRRR
+    - birthdate (datetime)
     - age (int) - vypocitany z data narozeni
     - location (Location) ve formatu latitude longitude
-pokud uzivatel zada hodnotu se spatnym datovym typem, tak vyhodite vyjimku
 
 2. Vytvorte tridu Location, ktera bude obsahovat nasledujici parametry
     - latitude (float)
     - longitude (float)
     - wkt (str) - bude obsahovat geometrii prevedenou na wkt (wkt - https://en.wikipedia.org/wiki/Well-known_text)
-    - city
+    - city (str)
 
-3. ziskejte od uzivatelu hodnoty pomoci input() a vytvorte jednotlive objekty
-4. implementujte funkce na vypis informaci o person i location objektech
+3. v ramci testovani funkcnosti si vytvorte objekt Person a objekt Location s hodnotami, které si vymyslíte
+4. implementujte funkce na vypis informaci o person i location objektech (__repr__)
 5. vytvorte funkci, ktera vypocita vek daneho cloveka podle data narozeni
 6. vytvorte funkci, ktera vytvori wkt bod z latitude a longitude
-7. napiste funkci na zmenu prijmeni urcite osoby
-8. zkuste vymyslet zpusob jak zavolat OpenStreetMap API pro ziskani mesta, kde clovek zije
+7. zkuste vymyslet zpusob jak zavolat OpenStreetMap API pro ziskani mesta, kde clovek zije
     https://nominatim.openstreetmap.org/
-
+Funkce spravne zaradte do trid.
 '''
 
 
