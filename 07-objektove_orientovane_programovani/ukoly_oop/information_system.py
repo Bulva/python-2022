f89638230ceee03d49e3d0e9098e6c3a16810879
Petr Silhak <petrsilhak@gmail.com>
2022-12-10 18:03:55 +0100

    is

--- 07-objektove_orientovane_programovani/ukoly_oop/information_system.py
+++ 07-objektove_orientovane_programovani/ukoly_oop/information_system.py
@@ -12,7 +12,7 @@ V tomto cviceni si vytvorime jednoduchy system pro ukladani informaci o lidech v
 2. Vytvorte tridu Location, ktera bude obsahovat nasledujici parametry
     - latitude (float)
     - longitude (float)
-    - wkt (str) - bude obsahovat geometrii prevedenou na wkt (wkt - https://en.wikipedia.org/wiki/Well-known_text)
+    - wkt (str) - bude obsahovat geometrii prevedenou na wkt (wkt - https://en.wikipediawikipedia.org/wiki/Well-known_text)
     - city (str)
 
 3. v ramci testovani funkcnosti si vytvorte objekt Person a objekt Location s hodnotami, které si vymyslíte
@@ -23,5 +23,45 @@ V tomto cviceni si vytvorime jednoduchy system pro ukladani informaci o lidech v
     https://nominatim.openstreetmap.org/
 Funkce spravne zaradte do trid.
 '''
+from datetime import datetime, date
+from random import randint
 
 
+class Person:
+    def __init__(self, name, surname, birthdate, location) -> None:
+        self.id = randint(1, 9999)
+        self.name = name
+        self.surname = surname
+        self.birthdate = birthdate
+        today = date.today()
+        self.age = today.year - birthdate.year - \
+            ((today.month, today.day) < (birthdate.month, birthdate.day))
+        self.location = location
+
+    def __repr__(self) -> str:
+        return f'Person -> id: {self.id}, name: {self.name}, surname: {self.surname}, birtdate: {self.birthdate}, age: {self.age}, location: {self.location}'
+
+
+class Location:
+    def __init__(self, latitude, longitude) -> None:
+        self.latitude = latitude
+        self.longitude = longitude
+        self.wkt = ''
+        #self.wkt = f'POINT ({self.latitude} {self.longitude})'
+        self.city = ''
+
+    def create_wkt(self):
+        return f'POINT ({self.latitude} {self.longitude})'
+
+    def __repr__(self) -> str:
+        return f'Location -> latitude: {self.latitude}, longitude: {self.longitude}, wkt: {self.wkt}, city: {self.city}'
+        #'Location latitude: {0}, longitude: {1}'.format(self.latitude, self.longitude)
+        # return  'Location -> latitude: ' + str(self.latitude) + ', longitude: ' + str(self.longitude) + ''
+
+
+loc = Location(52.5456465, 72.5465465)
+p = Person('Petr', 'Silhak', datetime(1993, 7, 5), Location(52.5456465, 72.5465465))
+p2 = Person('Jan', 'Horak', datetime(1993, 7, 5), Location(52.5456465, 72.5465465))
+print(loc.create_wkt())
+print(Location(52.5456465, 72.5465465).create_wkt())
+print(p2)
